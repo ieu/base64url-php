c514edc57eec65bea5dcc2cdbe682df99bab084c
ieu <5358482+ieu@users.noreply.github.com>
2019-12-03 21:23:57 +0800

    IMPROVE: Benchmark

--- benchmarks/SimpleDecodingBench.php
+++ benchmarks/SimpleDecodingBench.php
@@ -2,18 +2,29 @@
 
 
 use Ieu\Base64Url\Base64Url;
+use PhpBench\Benchmark\Metadata\Annotations\BeforeMethods;
 use PhpBench\Benchmark\Metadata\Annotations\Iterations;
 use PhpBench\Benchmark\Metadata\Annotations\Revs;
 
+/**
+ * @BeforeMethods("init")
+ */
 class SimpleDecodingBench
 {
+    private $encodedString;
+
+    public function init()
+    {
+        $this->encodedString = strtr(base64_encode(openssl_random_pseudo_bytes(mt_rand(0x10, 0x400))), '+/', '-_');
+    }
+
     /**
      * @Revs(1000)
      * @Iterations(10)
      */
     public function benchBase64Url()
     {
-        Base64Url::decode("SGVsbG8sIHdvcmxkIQ==");
+        Base64Url::decode($this->encodedString);
     }
 
     /**
@@ -23,6 +34,6 @@ class SimpleDecodingBench
     public function benchSimpleWrapper()
     {
         // https://github.com/firebase/php-jwt/blob/master/src/JWT.php
-        base64_decode(strtr("SGVsbG8sIHdvcmxkIQ==", '-_', '+/'));
+        base64_decode(strtr($this->encodedString, '-_', '+/'));
     }
 }
--- benchmarks/SimpleEncodingBench.php
+++ benchmarks/SimpleEncodingBench.php
@@ -2,18 +2,29 @@
 
 
 use Ieu\Base64Url\Base64Url;
+use PhpBench\Benchmark\Metadata\Annotations\BeforeMethods;
 use PhpBench\Benchmark\Metadata\Annotations\Iterations;
 use PhpBench\Benchmark\Metadata\Annotations\Revs;
 
+/**
+ * @BeforeMethods("init")
+ */
 class SimpleEncodingBench
 {
+    private $rawString;
+
+    public function init()
+    {
+        $this->rawString = openssl_random_pseudo_bytes(mt_rand(0x10, 0x400));
+    }
+
     /**
      * @Revs(1000)
      * @Iterations(10)
      */
     public function benchBase64Url()
     {
-        Base64Url::encode("Hello, world!");
+        Base64Url::encode($this->rawString);
     }
 
     /**
@@ -23,6 +34,6 @@ class SimpleEncodingBench
     public function benchSimpleWrapper()
     {
         // https://github.com/firebase/php-jwt/blob/master/src/JWT.php
-        strtr(base64_encode("Hello, world!"), '+/', '-_');
+        strtr(base64_encode($this->rawString), '+/', '-_');
     }
 }
